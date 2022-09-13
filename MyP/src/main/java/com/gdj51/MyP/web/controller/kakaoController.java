383ed0219e04325bab2325930405b174f620931d
배형석 <bhs06080@naver.com>
2022-09-13 15:43:23 +0900

    kakaoController Testing

---
+++ MyP/src/main/java/com/gdj51/MyP/web/controller/kakaoController.java
@@ -0,0 +1,15 @@
+package com.gdj51.MyP.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.aspectj.apache.bcel.classfile.annotation.NameValuePair;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class kakaoController {
+}
