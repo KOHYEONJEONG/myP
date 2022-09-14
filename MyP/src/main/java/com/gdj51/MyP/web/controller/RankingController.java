f39996731a0a6918f0da7628008fcbc02710e7cc
원석 <wkdrnrk685@naver.com>
2022-09-14 14:32:38 +0900

    원석

--- MyP/src/main/java/com/gdj51/MyP/web/controller/RankingController.java
+++ MyP/src/main/java/com/gdj51/MyP/web/controller/RankingController.java
@@ -1,11 +1,29 @@
 package com.gdj51.MyP.web.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gdj51.MyP.web.dao.IACDao;
+import com.gdj51.MyP.web.service.IJoinService;
 
 @Controller
 public class RankingController {
+	
+	@Autowired
+	public IJoinService mailService;
+	
+	@Autowired
+	public IACDao dao;
 
 	@RequestMapping(value = "/rankingFeeReasonableBoard")
 	public ModelAndView rankingFeeReasonableBoard(ModelAndView mav) {
@@ -13,10 +31,22 @@ public class RankingController {
 		return mav;
 	}
 
-	@RequestMapping(value = "/rankingSystemGoodParking")
-	public ModelAndView rankingSystemGoodParking(ModelAndView mav) {
-		mav.setViewName("ranking/rankingSystemGoodParking");
-		return mav;
+	@RequestMapping(value = "/rankingSystemGoodParking",
+			method = RequestMethod.POST, 
+			produces = "text/json;charset=UTF-8")
+	@ResponseBody
+	public String rankingFeeReasonableBoard(
+			@RequestParam HashMap<String, String> params) throws Throwable {
+		ObjectMapper mapper = new ObjectMapper();
+		
+		Map<String, Object> model = new HashMap<String, Object>();
+		
+		List<HashMap<String, String>> list = dao.getList("rank.getCaList", params);
+		
+		model.put("list", list);
+		
+		return mapper.writeValueAsString(model);
 	}
-
+	
+	
 }
