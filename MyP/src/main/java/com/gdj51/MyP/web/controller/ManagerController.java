f37f2f63ce8e02a96011c14b4355f581bfd079b4
saehee15 <sae_hwi@naver.com>
2022-09-15 14:37:27 +0900

    카테고리 목록 수정

--- MyP/src/main/java/com/gdj51/MyP/web/controller/ManagerController.java
+++ MyP/src/main/java/com/gdj51/MyP/web/controller/ManagerController.java
@@ -81,7 +81,7 @@ public class ManagerController {
 		// 페이지 받아오게 되어있음
 		int cnt = dao.getIntData("cate.getCateCnt", params);
 
-		HashMap<String, Integer> pd = ips.getPagingData(Integer.parseInt(params.get("page")), cnt, 5, 5);
+		HashMap<String, Integer> pd = ips.getPagingData(Integer.parseInt(params.get("page")), cnt, 10, 5);
 
 		params.put("start", Integer.toString(pd.get("start")));
 		params.put("end", Integer.toString(pd.get("end")));
@@ -90,6 +90,63 @@ public class ManagerController {
 
 		model.put("list", list);
 		model.put("pd", pd);
+
+		return mapper.writeValueAsString(model);
+	}
+
+	@RequestMapping(value = "/divManagementList", method = RequestMethod.POST, produces = "text/json;charset=UTF-8")
+	@ResponseBody
+	public String divManagementListAjax(@RequestParam HashMap<String, String> params) throws Throwable {
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> model = new HashMap<String, Object>();
+
+		// 페이지 받아오게 되어있음
+		int cnt = dao.getIntData("div.getDivCnt", params);
+
+		HashMap<String, Integer> pd = ips.getPagingData(Integer.parseInt(params.get("page")), cnt, 10, 5);
+
+		params.put("start", Integer.toString(pd.get("start")));
+		params.put("end", Integer.toString(pd.get("end")));
+
+		List<HashMap<String, String>> list = dao.getList("div.getDivList", params);
+
+		model.put("list", list);
+		model.put("pd", pd);
+
+		return mapper.writeValueAsString(model);
+	}
+
+	@RequestMapping(value = "/divManagementAction/{gbn}", method = RequestMethod.POST, produces = "text/json;charset=UTF-8")
+	@ResponseBody
+	public String divManagementActionAjax(@PathVariable String gbn, @RequestParam HashMap<String, String> params)
+			throws Throwable {
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> model = new HashMap<String, Object>();
+
+		int cnt = 0;
+
+		try {
+			switch (gbn) {
+			case "insert":
+				cnt = dao.insert("div.insertDiv", params);
+				break;
+			case "update":
+				cnt = dao.update("div.updateDiv", params);
+				break;
+			case "delete":
+				cnt = dao.update("div.deleteDiv", params);
+				break;
+			}
+
+			if (cnt > 0) {
+				model.put("msg", "success");
+			} else {
+				model.put("msg", "fail");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.put("msg", "error");
+		}
 
 		return mapper.writeValueAsString(model);
 	}
@@ -117,32 +174,32 @@ public class ManagerController {
 
 	// 관리자 페이지 회원관리 목록화면
 	@RequestMapping(value = "/memManagement")
-	public ModelAndView memManagement(@RequestParam HashMap<String, String> params,ModelAndView mav) {
-		
-		int page = 1;//첫페이지로 나타내려고
-		
-		if(params.get("page")!= null && params.get("page") != "") {
+	public ModelAndView memManagement(@RequestParam HashMap<String, String> params, ModelAndView mav) {
+
+		int page = 1;// 첫페이지로 나타내려고
+
+		if (params.get("page") != null && params.get("page") != "") {
 			page = Integer.parseInt(params.get("page"));
 		}
-		//페이지 번호
+		// 페이지 번호
 		mav.addObject("page", page);
-		
+
 		mav.setViewName("manager/memManagement");
 		return mav;
 	}
-	
-	//Ajax를 붙여야지 AOP에서 관리 안함.
+
+	// Ajax를 붙여야지 AOP에서 관리 안함.
 	@RequestMapping(value = "/memManagementListAjax", method = RequestMethod.POST, produces = "text/json;charset=UTF-8")
 	@ResponseBody
-	public String memManagementListAjax(@RequestParam HashMap<String,String> params) throws Throwable{
+	public String memManagementListAjax(@RequestParam HashMap<String, String> params) throws Throwable {
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> model = new HashMap<String, Object>();
-		
-		System.out.println("params : "+params.toString());
-		
+
+		System.out.println("params : " + params.toString());
+
 		// 페이지 받아오게 되어있음
 		int cnt = dao.getIntData("member.getMemberCnt", params);
-		
+
 		HashMap<String, Integer> pd = ips.getPagingData(Integer.parseInt(params.get("page")), cnt, 5, 5);
 
 		params.put("start", Integer.toString(pd.get("start")));
@@ -152,10 +209,9 @@ public class ManagerController {
 
 		model.put("list", list);
 		model.put("pd", pd);
-		
+
 		return mapper.writeValueAsString(model);
 	}
-	
 
 	// 관리자 페이지 신고관리 목록화면
 	@RequestMapping(value = "/reportReviewManagement")
