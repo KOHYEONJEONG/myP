1e52705fdf6264f6fcb9574deb02d7d95842fb0d
원석 <wkdrnrk685@naver.com>
2022-09-16 16:09:53 +0900

    FAQ

--- MyP/src/main/java/com/gdj51/MyP/web/controller/PartiNoticeController.java
+++ MyP/src/main/java/com/gdj51/MyP/web/controller/PartiNoticeController.java
@@ -1,15 +1,28 @@
 package com.gdj51.MyP.web.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gdj51.MyP.common.service.IPagingService;
 import com.gdj51.MyP.web.dao.IACDao;
 
 @Controller
 public class PartiNoticeController {
-	@Autowired IACDao iACDao;
+	@Autowired 
+	public IACDao iACDao;
+	
+	@Autowired
+	public IPagingService ips;
 	
 	
 	@RequestMapping(value = "/notice")
@@ -17,10 +30,20 @@ public class PartiNoticeController {
 		mav.setViewName("partiNotice/notice");
 		return mav;
 	}
-	
-	@RequestMapping(value = "/faq")
-	public ModelAndView faq(ModelAndView mav) {
+
+	@RequestMapping(value= "/faq")
+	public ModelAndView faq(
+			@RequestParam HashMap<String,String> params,
+			ModelAndView mav) {
+		int page = 1;
+		
+		if(params.get("page")!=null && params.get("page")!="") {
+		page = Integer.parseInt(params.get("page"));
+		}
+		mav.addObject("page",page);
+		
 		mav.setViewName("partiNotice/faqList");
+				
 		return mav;
 	}
 	
