3c203704e4b98be4e4d44afad24db908ea355e80
saehee15 <sae_hwi@naver.com>
2022-10-06 17:52:03 +0900

    카테고리 수정중

--- MyP/src/main/java/com/gdj51/MyP/web/controller/BookmarkController.java
+++ MyP/src/main/java/com/gdj51/MyP/web/controller/BookmarkController.java
@@ -21,9 +21,10 @@ public class BookmarkController {
 	@Autowired
 	public IACDao dao;
 
+	// 문화생활 즐겨찾기 삽입, 수정
 	@RequestMapping(value = "/cultureBookmarkAction/{gbn}", method = RequestMethod.POST, produces = "text/json;charset=UTF-8")
 	@ResponseBody
-	public String noticeAction(@PathVariable String gbn, @RequestParam HashMap<String, String> params)
+	public String cultureBookmarkAction(@PathVariable String gbn, @RequestParam HashMap<String, String> params)
 			throws Throwable {
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> model = new HashMap<String, Object>();
@@ -57,13 +58,63 @@ public class BookmarkController {
 	// 문화생활 즐겨찾기 리스트
 	@RequestMapping(value = "/cultureBookmarkList", method = RequestMethod.POST, produces = "text/json;charset=UTF-8")
 	@ResponseBody
-	public String noticeListAjax(@RequestParam HashMap<String, String> params) throws Throwable {
+	public String cultureBookmarkListAjax(@RequestParam HashMap<String, String> params) throws Throwable {
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> model = new HashMap<String, Object>();
 
 		System.out.println("bookmarkparams" + params);
 
 		List<HashMap<String, String>> list = dao.getList("bookmark.getCultrueBookmarkList", params);
+
+		model.put("list", list);
+
+		return mapper.writeValueAsString(model);
+	}
+
+	// 영화관 즐겨찾기 삽입, 수정
+	@RequestMapping(value = "/cinemaBookmarkAction/{gbn}", method = RequestMethod.POST, produces = "text/json;charset=UTF-8")
+	@ResponseBody
+	public String cinemaBookmarkAction(@PathVariable String gbn, @RequestParam HashMap<String, String> params)
+			throws Throwable {
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> model = new HashMap<String, Object>();
+
+		int cnt = 0;
+		System.out.println("params" + params);
+
+		try {
+			switch (gbn) {
+			case "insert":
+				cnt = dao.insert("bookmark.insertCinema", params);
+				break;
+			case "delete":
+				cnt = dao.delete("bookmark.deleteCinema", params);
+				break;
+			}
+
+			if (cnt > 0) {
+				model.put("msg", "success");
+			} else {
+				model.put("msg", "fail");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.put("msg", "error");
+		}
+
+		return mapper.writeValueAsString(model);
+	}
+
+	// 영화관 즐겨찾기 리스트
+	@RequestMapping(value = "/cinemaBookmarkList", method = RequestMethod.POST, produces = "text/json;charset=UTF-8")
+	@ResponseBody
+	public String cinemaBookmarkListAjax(@RequestParam HashMap<String, String> params) throws Throwable {
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> model = new HashMap<String, Object>();
+
+		System.out.println("bookmarkparams" + params);
+
+		List<HashMap<String, String>> list = dao.getList("bookmark.getCinemaBookmarkList", params);
 
 		model.put("list", list);
 
